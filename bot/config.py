deff301f701bdfb019e951b2291ee4c38e2ff5ab
Saitama3823 <117002587+Saitama3823@users.noreply.github.com>
2023-06-01 13:47:44 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,30 +1,17 @@
-#    This file is part of the Compressor distribution.
 #    Copyright (c) 2021 Danish_00
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, version 3.
-#
-#    This program is distributed in the hope that it will be useful, but
-#    WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-#    General Public License for more details.
-#
-# License can be found in <
-# https://github.com/1Danish-00/CompressorQueue/blob/main/License> .
+#    Improved By @Zylern
 
 from decouple import config
 
 try:
-    APP_ID = config("APP_ID", cast=int)
-    API_HASH = config("API_HASH")
-    BOT_TOKEN = config("BOT_TOKEN")
-    DEV = 1664850827
-    OWNER = config("OWNER")
+    APP_ID = "8440502"
+    API_HASH = "e77474ae3075f4000d3418c5a5a3a112"
+    BOT_TOKEN = "5521953154:AAETsXLlh354SXv6gpHbR47Jjx97-wHoRk4"
+    DEV = "1315975369 , 5480563272"
+    OWNER = "5480563272"
     ffmpegcode = ["-preset faster -c:v libx265 -s 854x480 -x265-params 'bframes=8:psy-rd=1:ref=3:aq-mode=3:aq-strength=0.8:deblock=1,1' -metadata 'title=Encoded By TGVid-Comp (https://github.com/Zylern/TGVid-Comp)' -pix_fmt yuv420p -crf 30 -c:a libopus -b:a 32k -c:s copy -map 0 -ac 2 -ab 32k -vbr 2 -level 3.1 -threads 1"]
-    THUMB = config("THUMBNAIL")
+    THUMBNAIL = "https://graph.org/file/a0185fe78369f993a8e97.jpg"
 except Exception as e:
-    LOGS.info("Environment vars Missing")
-    LOGS.info("something went wrong")
-    LOGS.info(str(e))
+    print("Environment vars Missing! Exiting App.")
+    print(str(e))
     exit(1)
